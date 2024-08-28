db5e194eed990758f014202784823a3edcb24ba5
Romeo <pribyllromeo@gmail.com>
2024-08-28 20:13:03 +0200

    repeatString function and test case implemented

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if (num < 0) return "ERROR";
+    let repeatedText = "";
+    for (let i = 0; i < num; i++) {
+        repeatedText += text;
+    }
+    return repeatedText;
 };
 
 // Do not edit below this line
