7a52d57afb957e7c55cf77414ed27b68c6a511ff
Romeo <pribyllromeo@gmail.com>
2024-08-28 20:46:21 +0200

    sumAll function and test case implemented

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let result = 0;
 
-};
+    if (start < 0 || end < 0) return 'ERROR';
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return 'ERROR';
+
+    if (start < end) {
+      for (let i = start; i <= end; i++) {
+        result += i;
+      }
+      return result;
+    } else {
+      for (let i = end; i <= start; i++) {
+        result += i;
+      }
+      return result;
+    }
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
